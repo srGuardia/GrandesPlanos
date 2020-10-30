74d9ac46e23570bf4c0b43c17edec3350ac5e898
breno guardia <brenoguardia02@gmail.com>
2020-10-30 08:45:46 -0300

    configuração cliente

--- tanquevendas/src/environments/environment.prod.ts
+++ tanquevendas/src/environments/environment.prod.ts
@@ -1,12 +1,13 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: "AIzaSyAfamH24A1YVJ3_QxZ9Pn9t_K4XbpRyfqI",
-    authDomain: "tanquevendas.firebaseapp.com",
-    databaseURL: "https://tanquevendas.firebaseio.com",
-    projectId: "tanquevendas",
-    storageBucket: "tanquevendas.appspot.com",
-    messagingSenderId: "136440818165",
-    appId: "1:136440818165:web:bf332c9312bf0643bb18bc"
-  }
+    apiKey: "AIzaSyD-P9YE_Q2uAyEBYMXBNjdwzGLng-oijyE",
+    authDomain: "tanquevendas-f6295.firebaseapp.com",
+    databaseURL: "https://tanquevendas-f6295.firebaseio.com",
+    projectId: "tanquevendas-f6295",
+    storageBucket: "tanquevendas-f6295.appspot.com",
+    messagingSenderId: "637426174111",
+    appId: "1:637426174111:web:90498d3f0008f1fc8a7aaa",
+    measurementId: "G-0E8PHQ9NY6",
+  },
 };
