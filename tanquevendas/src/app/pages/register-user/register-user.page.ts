f661e935f6cc75b78ba6159fc740522701dfae85
breno guardia <brenoguardia02@gmail.com>
2020-11-11 13:27:32 -0300

    ajustes

--- tanquevendas/src/app/pages/register-user/register-user.page.ts
+++ tanquevendas/src/app/pages/register-user/register-user.page.ts
@@ -106,6 +106,11 @@ export class RegisterUserPage implements OnInit {
       newOrganization.linkRegister = this.organizationSelect[0]._linkRegister;
       newOrganization.linkForecast = this.organizationSelect[0]._linkForecast;
       newOrganization.linkSales = this.organizationSelect[0]._linkSales;
+      newOrganization.active = this.organizationSelect[0]._active;
+      newOrganization.firstActionName = this.organizationSelect[0]._firstActionName;
+      newOrganization.firstActionLink = this.organizationSelect[0]._firstActionLink;
+      newOrganization.secondActionName = this.organizationSelect[0]._secondActionName;
+      newOrganization.secondActionLink = this.organizationSelect[0]._secondActionLink;
 
       newSheet.id = this.sheetSelect[0]._id;
       newSheet.nameSheet = this.sheetSelect[0]._nameSheet;
@@ -160,6 +165,11 @@ export class RegisterUserPage implements OnInit {
       newOrganization.linkRegister = this.organizationSelect[0]._linkRegister;
       newOrganization.linkForecast = this.organizationSelect[0]._linkForecast;
       newOrganization.linkSales = this.organizationSelect[0]._linkSales;
+      newOrganization.active = this.organizationSelect[0]._active;
+      newOrganization.firstActionName = this.organizationSelect[0]._firstActionName;
+      newOrganization.firstActionLink = this.organizationSelect[0]._firstActionLink;
+      newOrganization.secondActionName = this.organizationSelect[0]._secondActionName;
+      newOrganization.secondActionLink = this.organizationSelect[0]._secondActionLink;
 
       newSheet.id = this.sheetSelect[0]._id;
       newSheet.nameSheet = this.sheetSelect[0]._nameSheet;
