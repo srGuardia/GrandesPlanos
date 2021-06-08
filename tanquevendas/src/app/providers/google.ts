318d9413bd470a1f6434ef2aad90ab7159ff03c6
srGuardia <brenoguardia02@outlook.com>
2021-06-08 11:27:12 -0300

    identacao

--- tanquevendas/src/app/providers/google.ts
+++ tanquevendas/src/app/providers/google.ts
@@ -1,5 +1,5 @@
-import { Injectable } from "@angular/core";
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable()
 export class GoogleProvider {
@@ -8,19 +8,19 @@ export class GoogleProvider {
   constructor(public http: HttpClient) {}
 
   getHeaders(): HttpHeaders {
-    let headers = new HttpHeaders();
+    const headers = new HttpHeaders();
 
-    headers.append("Access-Control-Allow-Origin", "*");
-    headers.append("Access-Control-Allow-Headers", "Content-Type");
+    headers.append('Access-Control-Allow-Origin', '*');
+    headers.append('Access-Control-Allow-Headers', 'Content-Type');
 
     return headers;
   }
 
   load(idSheet: string) {
     const url =
-      "https://spreadsheets.google.com/feeds/list/" +
+      'https://spreadsheets.google.com/feeds/list/' +
       idSheet +
-      "/od6/public/values?alt=json";
+      '/od6/public/values?alt=json';
 
     return this.http.get(url, { headers: this.getHeaders() }).toPromise();
   }
